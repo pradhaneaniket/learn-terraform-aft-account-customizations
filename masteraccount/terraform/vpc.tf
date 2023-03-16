b80d39bb82a08d1c6531975cc25910d2ff6d88d5
unknown <aniket.pradhane@blazeclan.com>
2023-03-16 20:39:39 +0530

    VPC in master account

--- masteraccount/terraform/vpc.tf
+++ masteraccount/terraform/vpc.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_vpc" "sharedService" {
-  cidr_block       = "10.0.1.0/24"
+  cidr_block       = "10.0.1.0/23"
 
   tags = {
     Name = "MasteAccountVPC"
