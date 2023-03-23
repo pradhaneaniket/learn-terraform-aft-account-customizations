61667e9e41495633327574034b3a769fd8c2f4e5
unknown <aniket.pradhane@blazeclan.com>
2023-03-23 18:12:30 +0530

    CT MGMT Account updates

--- masteraccount/terraform/bootstrap.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "ct-management-account-tf-state"
-    key            = "MGMT/terraform.tfstate"
-    region         = "eu-central-1"
-    # dynamodb_table = "ct-tf-state-lock"
-  }
-}
