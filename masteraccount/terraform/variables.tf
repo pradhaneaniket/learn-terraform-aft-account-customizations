c05960659a9ad9edfa4a6f90e88932504a8a8cc2
unknown <aniket.pradhane@blazeclan.com>
2023-03-23 15:44:14 +0530

    Create Permission Set for CloudAdmin Group

--- masteraccount/terraform/variables.tf
+++ masteraccount/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "ct_management_account_id" {}
 
-variable "log_archive_account_id" {}
+# variable "log_archive_account_id" {}
 
-variable "audit_account_id" {}
+# variable "audit_account_id" {}
 
-variable "aft_management_account_id" {}
+# variable "aft_management_account_id" {}
