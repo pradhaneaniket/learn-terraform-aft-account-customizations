136411dffe53fb85fac51ec6d74f6a9c558f6437
unknown <aniket.pradhane@blazeclan.com>
2023-03-23 12:15:18 +0530

    New Permission Set for Infra Admin

--- masteraccount/terraform/idc_premission_set.tf
+++ masteraccount/terraform/idc_premission_set.tf
@@ -1,6 +1,15 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_ssoadmin_permission_set" "aws_cloud_admin_permission_set" {
   name         = "AWSCloudAdmin"
   description  = "Permission set for AWS Cloud Admin user/group"
+  instance_arn = tolist(data.aws_ssoadmin_instances.idc_instance.arns)[0]
+  session_duration = "PT1H"
+}
+
+resource "aws_ssoadmin_permission_set" "aws_infra_admin_permission_set" {
+  name         = "AWSInfradmin"
+  description  = "Permission set for AWS Infra Admin user/group"
   instance_arn = tolist(data.aws_ssoadmin_instances.idc_instance.arns)[0]
   session_duration = "PT1H"
 }
