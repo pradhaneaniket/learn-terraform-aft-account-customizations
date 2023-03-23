dc7072a7034f7d9b1b964f55ddbf0fefd80001e1
unknown <aniket.pradhane@blazeclan.com>
2023-03-23 16:58:39 +0530

    Create Permission Set for CloudAdmin Group

--- masteraccount/terraform/main.tf
+++ masteraccount/terraform/main.tf
@@ -1,8 +1,8 @@
 module "permissionSet" {
-  source = "./module/IAM"
+  source = "./modules/IAM"
   ct_management_account_id = var.ct_management_account_id
 }
 module "ct_management_account_vpc" {
-  source = "./module/VPC"
+  source = "./modules/VPC"
   ct_management_account_vpc = var.ct_management_account_vpc
 }
